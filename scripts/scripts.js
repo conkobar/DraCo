8c23ab3f71acade804e3d8313f3abaf46688d56c
mdallen5393 <allen_matthew@outlook.com>
2023-04-12 16:31:30 -0500

    Tried to get database to work

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import {
-  getFirestore,
-  collection,
-  getDocs,
-} from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyDhnBjqqa7oQ9jASQxQZLKHz8QiDcq0Daw',
   authDomain: 'holberton-draco.firebaseapp.com',
@@ -17,19 +5,23 @@ const firebaseConfig = {
   storageBucket: 'holberton-draco.appspot.com',
   messagingSenderId: '316650761146',
   appId: '1:316650761146:web:e178596d92e83e469f4b83',
-  measurementId: 'G-0VG0RJPC0V',
+  measurementId: 'G-0VG0RJPC0V'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(app);
 
-// Get a reference to the 'Products' collection
-const productsRef = collection(db, 'Products');
+// Get the unordered list element
+var ul = document.getElementById("product-list");
 
-// Get all documents from the 'Products' collection
-// getDocs(productsRef).then((snapshot) => {
-//   console.log(snapshot);
-// });
-
-console.log(productsRef);
+// Get all documents from the Products collection
+db.collection("Products").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    // Create a new list item element
+    var li = document.createElement("li");
+    // Set the text content to the product name
+    li.textContent = doc.data().name;
+    // Append the list item to the unordered list
+    ul.appendChild(li);
+  });
+});
